a4ca9a2b5f0b346708749e48f00d1333a9c6d7e9
goel-vanshika <goelvanshika1006@gmail.com>
2023-02-15 09:35:25 +0530

    Commit 3

---
+++ main/Pipeline_484.cpp
@@ -0,0 +1,12 @@
+//File: Pipeline_484.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "This is after Webhooks were added\n.";
+  cout << "This is after the Pipeline script in Groovy task was done.\n";
+  return 0;
+}
